cc086b346105e697288c7c79da6f8b075348e3a8
hms5960 <hms5960@naver.com>
2023-03-19 15:09:58 +0900

    1일

---
+++ demo/src/main/java/kr/ac/jejunu/user/User.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu.user;
+
+public class User {
+    private long id;
+    private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ demo/src/test/java/kr/ac/jejunu/user/userDaoTests.java
@@ -0,0 +1,22 @@
+package kr.ac.jejunu.user;
+
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+public class userDaoTests {
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        long id= 1l;
+        String name = "baba";
+        String password = "1234";
+        UserDao userDao = new UserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+}
